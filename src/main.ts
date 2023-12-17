9b43db62cde39aa6a985c05860015cac0113f9bf
Sarthak Jain <sarthak.jain@comprotechnologies.com>
2023-12-17 13:33:48 +0530

    updated port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
